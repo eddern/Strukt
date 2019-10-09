2127b8b674c48bb53c09710aa0079cde85f6cb04
Edvard Schreiner Sjøblom <edsc@netlight.com>
2019-10-09 15:59:01 +0200

    implemented NativeBase

--- pages/Loading.tsx
+++ pages/Loading.tsx
@@ -1,29 +1,47 @@
-import React, { useEffect } from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { AppLoading } from 'expo';
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { Container, Text } from 'native-base';
 
-import firebase from '../utils/firebase';
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center',
-    },
-});
+import firebase from './../utils/firebase';
 
 const LoadingComponent = ({ navigation }: { navigation: NavigationScreenProp<NavigationState>}) => {
-    useEffect(() => {
-        firebase.auth().onIdTokenChanged( (user) => {
-            user ? navigation.navigate('Home') : navigation.navigate('SignUp');
+    const [fontReady, setFontReady] = useState(false)
+    const [navigationRoute, setNavigationRoute] = useState('')
+
+    // Initialize fonts
+    const loadFont = async () => {
+        await Font.loadAsync({
+            Roboto: require('./../assets/fonts/Roboto-Regular.ttf'),
+            Roboto_medium: require('./../assets/fonts/Roboto-Medium.ttf'),
+            ...Ionicons.font,
         });
+        setFontReady(true);
+    }
+
+    const checkUserAndSetNavigationPath = () => {
+        firebase.auth().onIdTokenChanged( (user) => {
+            user ? setNavigationRoute('Home') : setNavigationRoute('SignUp');
+        })
+    }
+
+    // Load font and check User first time the component mounts
+    useEffect(() => {
+        checkUserAndSetNavigationPath();
+        loadFont();
     }, []);
 
+    // Check if font is ready, and redirect to appropriate page
+    useEffect(() => {
+        if (fontReady) {
+            navigation.navigate(navigationRoute);
+        }
+    })
+
     return (
-        <View style={styles.container}>
-            <Text>Loading</Text>
-            <ActivityIndicator size="large"></ActivityIndicator>
-        </View>
+        <AppLoading />
     );
 };
 
--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -1,67 +1,102 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-
-import firebase from '../utils/firebase';
+import React, { useState, useEffect } from 'react';
+import { Button, Container, Content, Form, Item, Input, Label, Text, Toast } from 'native-base';
+import { StyleSheet } from 'react-native'
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
 
-const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center',
-    },
-    textInput: {
-        borderColor: 'gray',
-        borderWidth: 1,
-        height: 40,
-        marginTop: 8,
-        width: '90%',
-    },
-});
+import firebase from '../utils/firebase';
+import CustomHeader from '../components/Header'
 
+const styles = StyleSheet.create({
+    button: {
+        width: '20%',
+        left: 120,
+    },
+})
 const SignUp = ({navigation }: { navigation: NavigationScreenProp<NavigationState>}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
     const handleSignUp = () => {
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch( (err) => setErrorMessage(err.message)) ;
+        if (password2 === password) {
+            firebase.auth()
+                .createUserWithEmailAndPassword(email, password)
+                .catch( (err) => setErrorMessage(err.message))
+        } else {
+            setErrorMessage('Passwords does not match');
+
+        }
     };
+
+    useEffect(() => {
+        if (errorMessage) {
+            Toast.show({
+                text: errorMessage,
+                buttonText: 'Okay',
+                duration: 3000,
+            })
+        }
+    }, [errorMessage]);
 
     const toLogin = () => {
         navigation.navigate('Login');
     };
 
     return (
-        <View style={styles.container}>
-            <Text>Sign Up</Text>
-            { errorMessage.length ?
-                <Text style={{ color: 'red' }}>
-                    {errorMessage}
-                </Text>
-                :
-                <></>
-            }
-            <TextInput
-                placeholder="Email"
-                autoCapitalize="none"
-                style={styles.textInput}
-                onChangeText={(emailText) => setEmail(emailText)}
-                value={email}
-            />
-            <TextInput
-                secureTextEntry
-                placeholder="Password"
-                autoCapitalize="none"
-                style={styles.textInput}
-                onChangeText={(passwordFieldText) => setPassword(passwordFieldText)}
-                value={password}
-            />
-        <Button title="Sign Up" onPress={handleSignUp} />
-        <Button
-            title="Already have an account? Login"
-            onPress={toLogin} />
-        </View>
+        <Container>
+            <CustomHeader pageTitle="Sign Up"/>
+            <Content>
+                <Form>
+                    <Item fixedLabel>
+                        <Label>Email:</Label>
+                        <Input
+                            placeholder="my@email.ofc"
+                            autoFocus
+                            autoCapitalize="none"
+                            textContentType="emailAddress"
+                            keyboardType="email-address"
+                            value={email}
+                            onChangeText={setEmail}
+                            />
+                    </Item>
+                    <Item fixedLabel>
+                        <Label>Password:</Label>
+                        <Input
+                            placeholder="new password"
+                            textContentType="newPassword"
+                            secureTextEntry
+                            value={password}
+                            onChangeText={setPassword}
+                            />
+                    </Item>
+                    <Item fixedLabel>
+                        <Label>Confirm:</Label>
+                        <Input
+                            placeholder="new password"
+                            textContentType="newPassword"
+                            secureTextEntry
+                            value={password2}
+                            onChangeText={setPassword2}
+                            />
+                    </Item>
+                </Form>
+                <Button
+                    block
+                    style={{marginTop: 5, marginBottom: 5}}
+                    onPress={handleSignUp}
+                    >
+                        <Text> Sign up </Text>
+                </Button>
+                <Button
+                    full
+                    transparent
+                    onPress={toLogin}
+                    >
+                    <Text> Already signed up? Log in instead </Text>
+                </Button>
+            </Content>
+        </Container>
     );
 
 };
